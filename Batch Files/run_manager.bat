bf5799ed663e64d493cb31c557c418b59db0ee42
Ricky Schweky <Ricky@Simpletowork.com>
2023-10-17 08:57:57 -0400

    updates

---
+++ Batch Files/run_manager.bat
@@ -0,0 +1,4 @@
+cd C:\
+"C:\Users\%USERNAME%\Desktop\New Projects\YGB Group\YGB-Group-Quickbase\venv\scripts\python.exe" "C:\Users\%USERNAME%\Desktop\New Projects\YGB Group\YGB-Group-Quickbase\Python Files\program_manager.py"
+
+cmd/k
