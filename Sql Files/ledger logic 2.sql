14cd4aef617fbaddf0d7e83e3ca0e1847ac88ffd
Ricky <Ricky@simpletowork.com>
2023-10-24 08:45:29 -0400

    updates

--- Sql Files/ledger logic 2.sql
+++ Sql Files/ledger logic 2.sql
@@ -1,28 +1,14 @@
-set @start_date:= "2022-11-01";
-update ygb_quickbase_po_data set Eta = "2023-10-04" where PO_Status= "Completed" and ETA = "";
-
-
 drop table if exists ygb_quickbase_active_sku_list;
 create table if not exists ygb_quickbase_active_sku_list(primary key(account_name, sku))
 select distinct account_name, SKU from product_data
-where sku in (select distinct sku from ygb_quickbase_po_data where ETA >= @start_date);
-
-
-select * from ygb_quickbase_po_data;
--- where sku in ( "Exw-Fam-9871", "B2-Wood-652", "Bds-654", "Bds-754", "Bds-754-Wg", "B09Fyhmsy4-Newitem-Fba");
+where sku in ("B2-Wood-652",  "3W-9BHO-WF4J");
 
 drop table if exists ygb_inventory_ledger_summary;
 create table if not exists ygb_inventory_ledger_summary(PRIMARY KEY(ACCOUNT_NAME, MSKU, DATE, DISPOSITION))
-select A.ACCOUNT_NAME, DATE, ASIN, MSKU, DISPOSITION, SUM(`STARTING WAREHOUSE BALANCE`) AS `STARTING WAREHOUSE BALANCE`,
-SUM(RECEIPTS) AS RECEIPTS, 
-SUM(`CUSTOMER SHIPMENTS`) as `CUSTOMER SHIPMENTS`, 
-sum(`CUSTOMER RETURNS`) as `CUSTOMER RETURNS`, 
-sum(`VENDOR RETURNS`) as `VENDOR RETURNS`, 
-sum(`ENDING WAREHOUSE BALANCE`) as `ENDING WAREHOUSE BALANCE`
+select A.ACCOUNT_NAME, DATE, ASIN, MSKU, DISPOSITION, `STARTING WAREHOUSE BALANCE`, RECEIPTS, `CUSTOMER SHIPMENTS`, `CUSTOMER RETURNS`, `VENDOR RETURNS`, `ENDING WAREHOUSE BALANCE`
 from ledger_summary_view A
 inner join ygb_quickbase_active_sku_list B on A.account_name = B.account_name  and a.msku = b.sku
-where date >=  @start_date 
-group by ACCOUNT_NAME, MSKU, DATE, DISPOSITION;
+where date >=  @start_date ;
 
 SET @MAX_DATE = (SELECT MAX(DATE) FROM ledger_summary_view  );
 
@@ -67,14 +53,10 @@ drop table if exists ygb_inventory_ledger_receipts;
 create table if not exists ygb_inventory_ledger_receipts
 select B.*, ifnull(lag(Rolling_Qty, 1) over(partition by Account_Name, sku order by id),0) as Last_Rolling_qty from
 (select A.*, sum(QTY) over(partition by Account_Name, sku order by id) as Rolling_Qty from
-(select row_number() over (partition by A.Account_Name, A.sku order by  A.Account_Name, A.sku, ETA) as ID, A.Account_Name,FBA_Shipment_ID,ETA, A.sku, asin, "Received",  PO_Status, QTY  ,
-Unit_Price, Duties / QTY as Duties, Customs_Fees / QTY as Customs_Fees, Demmurage / QTY as Demmurage, Container_Cost/ QTY as Container_Cost, Trucking_Cost / QTY as Trucking_Cost
-
+(select row_number() over (partition by A.Account_Name, A.sku order by  A.Account_Name, A.sku, ETA) as ID, A.Account_Name,FBA_Shipment_ID,ETA, A.sku, asin, "Received",  PO_Status, QTY  
 from ygb_quickbase_po_data A
 inner join ygb_quickbase_active_sku_list B on A.account_name = b.account_name  and a.sku = b.sku
-where  PO_Status = "Completed" and FBA_Shipment_ID != "" and ETA >= @start_date) A) B;
-
-select * from ygb_quickbase_po_data where  PO_Status = "Completed" and ETA >= @start_date;
+where  PO_Status = "Completed" and FBA_Shipment_ID != "") A) B;
 
 
 drop table if exists ygb_inventory_ledger_removals;
@@ -117,16 +99,25 @@ update ygb_inventory_ledger_detail A inner join
 (select *, ifnull(lag(rolling_qty, 1) over(partition by account_name, sku order by  account_name, sku, date, `AMAZON-ORDER-ID`),0) as Last_Rolling from ygb_inventory_ledger_detail) B using(id)
 Set a.last_rolling_qty = B.Last_Rolling;
 
-drop table if exists ygb_inventory_ledger_assignment;
-create table if not exists ygb_inventory_ledger_assignment
-select A.*, B.FBA_Shipment_ID, B.Rolling_Qty as PO_Rolling_Qty, B.Last_Rolling_qty as PO_Last_Rolling_qty
+select A.*, B.FBA_Shipment_ID, B.Rolling_Qty, B.Last_Rolling_qty
 from ygb_inventory_ledger_detail A 
-left join ygb_inventory_ledger_receipts B on A.Account_Name = B.Account_Name and A.sku = B.sku and A.rolling_qty <= B.rolling_qty and greatest(A.rolling_qty,0) >= B.last_rolling_qty;
+left join ygb_inventory_ledger_receipts B on A.Account_Name = B.Account_Name and A.sku = B.sku and A.rolling_qty <= B.rolling_qty and A.rolling_qty > B.last_rolling_qty;
+
+
+
+-- select * from ygb_inventory_ledger_detail;
+-- select * from ygb_inventory_ledger_receipts;
 
 
 
 
 
-select * from ygb_inventory_ledger_receipts;
-select * from ygb_inventory_ledger_assignment where FBA_Shipment_ID is null;
-select * from ygb_quickbase_po_data
+-- -- select * from fba_removal_shipment_detail where  sku = "3W-9BHO-WF4J";
+
+-- select *  from 
+-- ledger_detail_view a LEFT JOIN ygb_inventory_ledger_summary b USING(ACCOUNT_NAME, MSKU, DATE, DISPOSITION) 
+-- WHERE  MSKU = "3W-9BHO-WF4J" and date >= "2022-11-01" and date <=  @MAX_DATE and `EVENT TYPE` not in ("WhseTransfers")
+-- and `EVENT TYPE` = "Receipts";
+
+
+
