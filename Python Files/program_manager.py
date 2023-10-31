9cd931bc9ada74e38da4bf16cb89b37b1ff3bd4e
Ricky Schweky <Ricky@Simpletowork.com>
2023-10-31 05:04:58 -0400

    updates

--- Python Files/program_manager.py
+++ Python Files/program_manager.py
@@ -54,9 +54,9 @@ def run_program(environment):
     executeScriptsFromFile(engine=engine, folder_name=sql_folder, file_name='data logic.sql')
     executeScriptsFromFile(engine=engine, folder_name=sql_folder, file_name='finances logic.sql')
 
-    # upload_product_data(x, engine)
-    # upload_sales_data(x, engine, start_date)
-    # upload_returns_data(x, engine, start_date)
+    upload_product_data(x, engine)
+    upload_sales_data(x, engine, start_date)
+    upload_returns_data(x, engine, start_date)
     #
     # upload_sales_fees_data(x, engine, start_date)
     #
